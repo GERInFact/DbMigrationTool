321e62a9610591be84db58845624365705a6b03a
M. Hille <marcel-andre.hille@eder.de>
2019-01-24 16:03:06 +0100

    Update

--- DataBaseMigrator/DataBaseMigrator/Form1.Designer.cs
+++ DataBaseMigrator/DataBaseMigrator/Form1.Designer.cs
@@ -44,13 +44,13 @@
             // gridDatabase
             // 
             this.gridDatabase.AllowUserToOrderColumns = true;
-            this.gridDatabase.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.gridDatabase.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.gridDatabase.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.gridDatabase.BackgroundColor = System.Drawing.Color.Maroon;
             this.gridDatabase.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.gridDatabase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridDatabase.Cursor = System.Windows.Forms.Cursors.Default;
+            this.gridDatabase.Dock = System.Windows.Forms.DockStyle.Right;
             this.gridDatabase.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             this.gridDatabase.GridColor = System.Drawing.Color.White;
             this.gridDatabase.Location = new System.Drawing.Point(716, 0);
@@ -60,7 +60,7 @@
             // 
             // txtSqlConnectionString
             // 
-            this.txtSqlConnectionString.Location = new System.Drawing.Point(13, 98);
+            this.txtSqlConnectionString.Location = new System.Drawing.Point(14, 48);
             this.txtSqlConnectionString.Name = "txtSqlConnectionString";
             this.txtSqlConnectionString.Size = new System.Drawing.Size(685, 20);
             this.txtSqlConnectionString.TabIndex = 4;
@@ -68,81 +68,102 @@
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(12, 124);
+            this.btnConnect.AutoSize = true;
+            this.btnConnect.BackColor = System.Drawing.Color.White;
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(13, 169);
             this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new System.Drawing.Size(685, 51);
             this.btnConnect.TabIndex = 5;
             this.btnConnect.Text = "Connect";
-            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.UseVisualStyleBackColor = false;
             this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
             // btnReload
             // 
-            this.btnReload.Location = new System.Drawing.Point(13, 205);
+            this.btnReload.AutoSize = true;
+            this.btnReload.BackColor = System.Drawing.Color.White;
+            this.btnReload.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReload.Location = new System.Drawing.Point(14, 250);
             this.btnReload.Name = "btnReload";
             this.btnReload.Size = new System.Drawing.Size(318, 51);
             this.btnReload.TabIndex = 6;
             this.btnReload.Text = "Reload";
-            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.UseVisualStyleBackColor = false;
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(379, 205);
+            this.btnUpdate.AutoSize = true;
+            this.btnUpdate.BackColor = System.Drawing.Color.White;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(380, 250);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(318, 51);
             this.btnUpdate.TabIndex = 7;
             this.btnUpdate.Text = "Update";
-            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.UseVisualStyleBackColor = false;
             // 
             // btnSerialize
             // 
-            this.btnSerialize.Location = new System.Drawing.Point(11, 391);
+            this.btnSerialize.AutoSize = true;
+            this.btnSerialize.BackColor = System.Drawing.Color.Black;
+            this.btnSerialize.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSerialize.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSerialize.Location = new System.Drawing.Point(13, 494);
             this.btnSerialize.Name = "btnSerialize";
             this.btnSerialize.Size = new System.Drawing.Size(318, 51);
             this.btnSerialize.TabIndex = 8;
             this.btnSerialize.Text = "Save Data as XML";
             this.btnSerialize.UseMnemonic = false;
-            this.btnSerialize.UseVisualStyleBackColor = true;
+            this.btnSerialize.UseVisualStyleBackColor = false;
             this.btnSerialize.Click += new System.EventHandler(this.btnSerialize_Click);
             // 
             // btnDeserialize
             // 
-            this.btnDeserialize.Location = new System.Drawing.Point(377, 391);
+            this.btnDeserialize.AutoSize = true;
+            this.btnDeserialize.BackColor = System.Drawing.Color.Black;
+            this.btnDeserialize.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeserialize.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDeserialize.Location = new System.Drawing.Point(379, 494);
             this.btnDeserialize.Name = "btnDeserialize";
             this.btnDeserialize.Size = new System.Drawing.Size(318, 51);
             this.btnDeserialize.TabIndex = 9;
             this.btnDeserialize.Text = "Load from XML";
             this.btnDeserialize.UseMnemonic = false;
-            this.btnDeserialize.UseVisualStyleBackColor = true;
+            this.btnDeserialize.UseVisualStyleBackColor = false;
             this.btnDeserialize.Click += new System.EventHandler(this.btnDeserialize_Click);
             // 
             // btnWriteToDb
             // 
-            this.btnWriteToDb.Location = new System.Drawing.Point(10, 475);
+            this.btnWriteToDb.AutoSize = true;
+            this.btnWriteToDb.BackColor = System.Drawing.Color.Black;
+            this.btnWriteToDb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWriteToDb.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnWriteToDb.Location = new System.Drawing.Point(12, 418);
             this.btnWriteToDb.Name = "btnWriteToDb";
-            this.btnWriteToDb.Size = new System.Drawing.Size(318, 51);
+            this.btnWriteToDb.Size = new System.Drawing.Size(685, 51);
             this.btnWriteToDb.TabIndex = 10;
             this.btnWriteToDb.Text = "Write to DB";
-            this.btnWriteToDb.UseVisualStyleBackColor = true;
+            this.btnWriteToDb.UseVisualStyleBackColor = false;
             this.btnWriteToDb.Click += new System.EventHandler(this.btnWriteToDb_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(221, 338);
+            this.label1.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(169, 364);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(245, 37);
+            this.label1.Size = new System.Drawing.Size(383, 34);
             this.label1.TabIndex = 11;
             this.label1.Text = "Data Integration";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(221, 47);
+            this.label2.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(192, 113);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(236, 37);
+            this.label2.Size = new System.Drawing.Size(360, 34);
             this.label2.TabIndex = 12;
             this.label2.Text = "Data Extraction";
             // 
@@ -150,6 +171,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.DarkGray;
             this.ClientSize = new System.Drawing.Size(1190, 626);
             this.Controls.Add(this.label2);
@@ -163,7 +185,7 @@
             this.Controls.Add(this.txtSqlConnectionString);
             this.Controls.Add(this.gridDatabase);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Database Migrator";
             ((System.ComponentModel.ISupportInitialize)(this.gridDatabase)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
---
+++ DataBaseMigrator/Models/agreement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class agreement
+    {
+        public int IdAgreement { get; set; }
+        public string Agreement { get; set; }
+    }
+}
